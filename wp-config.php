d0683160d23c15fe33237cecd99d4089e903e29a
luongyen <luongyen1508@gmail.com>
2019-05-03 16:36:26 +0700

    edit config

--- wp-config.php
+++ wp-config.php
@@ -17,7 +17,7 @@
  *
  * @package WordPress
  */
-header('X-Frame-Options: SAMPORIGIN');
+header('X-Frame-Options: SAMEORIGIN');
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define( 'DB_NAME', 'bzuwigtgxjfbgdrif9l6' );
